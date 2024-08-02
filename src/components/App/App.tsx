0d80f66495bd50a74006b7508203b579cd083bd9
Vladimir Strugov <Vovushik@MacBook-Air-Alexandra.local>
2024-08-02 16:08:21 +0300

    add buttons

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -4,6 +4,8 @@ import Title from '../Title/Title';
 import { MockData } from '../../utils/Data/MockData';
 import Years from '../Years/Years';
 import { MockDataInit } from '../../utils/Data/DataTypes';
+import InfoSlider from '../Slider/Slider';
+import ChooseSphereButtons from '../ChooseSphereButtons/ChooseSphereButtons';
 
 const App: React.FC = () => {
   const [mockData, setMockData] = useState<MockDataInit[]>(MockData);
@@ -13,6 +15,12 @@ const App: React.FC = () => {
     <div className="App">
       <Title />
       <Years data={mockData[selected]} />
+      <ChooseSphereButtons
+        currentSelected={selected}
+        setNewSelected={setSelected}
+        maxValue={MockData.length}
+      />
+      <InfoSlider data={mockData[selected]} />
     </div>
   );
 };
