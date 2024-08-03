31b9c07963f9ad7b79d28760dcb86e804b37dcae
Vladimir <strugov2001@yandex.ru>
2024-08-03 17:31:21 +0300

    refactor buttons

--- src/components/InfoSlider/InfoSlider.tsx
+++ src/components/InfoSlider/InfoSlider.tsx
@@ -32,8 +32,20 @@ const InfoSlider: React.FC<SliderProps> = ({ data }) => {
           </SwiperSlide>
         ))}
       </Swiper>
-      <button className="infoSlider__buttonPrev"></button>
-      <button className="infoSlider__buttonNext"></button>
+      <button type="button" className="infoSlider__buttonPrev">
+        <span className="infoSlider__buttonIcon">
+          <svg width="24" height="24" role="none" viewBox="0 0 16 16">
+            <path d="M9.6 4l-4 4 4 4 .8-.8L7.2 8l3.2-3.2z" fill="#3877EE"></path>
+          </svg>
+        </span>
+      </button>
+      <button type="button" className="infoSlider__buttonNext">
+        <span className="infoSlider__buttonIcon">
+          <svg width="24" height="24" role="none" viewBox="0 0 16 16">
+            <path d="M6.4 12l4-4-4-4-.8.8L8.8 8l-3.2 3.2z" fill="#3877EE"></path>
+          </svg>
+        </span>
+      </button>
     </div>
   );
 };
